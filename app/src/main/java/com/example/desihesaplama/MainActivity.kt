4b7bdb4ca3bd3557cec7f48a4a9df64431833a7e
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-03-12 23:01:26 +0300

    Back End Completed

--- app/src/main/java/com/example/desihesaplama/MainActivity.kt
+++ app/src/main/java/com/example/desihesaplama/MainActivity.kt
@@ -1,16 +1,64 @@
+@file:Suppress("DEPRECATION")
+
 package com.example.desihesaplama
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var thicknessEditText: EditText
+    private lateinit var widthEditText: EditText
+    private lateinit var priceEditText: EditText
+    private lateinit var heightEditText: EditText
+
+    private var result: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
+
+        thicknessEditText = findViewById(R.id.thickness)
+        widthEditText = findViewById(R.id.width)
+        priceEditText = findViewById(R.id.price)
+        heightEditText = findViewById(R.id.height)
+        val desiHesapla = findViewById<Button>(R.id.button2)
+        val priceHesapla = findViewById<Button>(R.id.button)
+
+        desiHesapla.setOnClickListener {
+            val thickness = thicknessEditText.text.toString().toIntOrNull() ?: 0
+            val width = widthEditText.text.toString().toIntOrNull() ?: 0
+            val height = heightEditText.text.toString().toIntOrNull() ?: 0
+            val desiHeader = "Desi Sonucu"
+
+            result = (thickness * width * height)
+            showAlertDialog(desiHeader, result)
+        }
+
+        priceHesapla.setOnClickListener {
+            val price = priceEditText.text.toString().toIntOrNull() ?: 0
+            val priceResult = result * price
+            val priceHeader = "Fiyat Sonucu"
+            showAlertDialog(priceHeader, priceResult)
+        }
+    }
+
+    @SuppressLint("UseCompatLoadingForDrawables")
+    private fun showAlertDialog(h1: String, s1: Int) {
+        val dialogBuilder = MaterialAlertDialogBuilder(this)
+        dialogBuilder.setTitle(h1)
+        dialogBuilder.setMessage("Desi Hesaplamanız: $s1")
+            .setPositiveButton("Tamam") { _, _ ->
+
+            }
+            .setCancelable(false)
+        val dialog = dialogBuilder.create()
+        dialog.show()
+
+        val customBackgroundDrawable = resources.getDrawable(R.drawable.custom_dialog_design)
+        dialog.window?.setBackgroundDrawable(customBackgroundDrawable)
     }
 }
