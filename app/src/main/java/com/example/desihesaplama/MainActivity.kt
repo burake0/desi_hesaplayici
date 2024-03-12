4deca693266759e72fdd0bc3f54e7a3847509623
Burak Eskisemerci <eskisemercib27@gmail.com>
2024-03-12 22:31:26 +0300

    UI Design Updated

--- app/src/main/java/com/example/desihesaplama/MainActivity.kt
+++ app/src/main/java/com/example/desihesaplama/MainActivity.kt
@@ -1,17 +1,16 @@
 package com.example.desihesaplama
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.dialog.MaterialAlertDialogBuilder
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContentView(R.layout.activity_main)
-
-
     }
 }
